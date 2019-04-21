25dd5316d7c3b46a69624afab32ed92d21d75471
Stephen Bondor <bondor@Stephens-MacBook-Pro.local>
2019-04-21 03:34:19 -0700

    Add comments

--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -5,3 +5,9 @@ function memes(parent, args, context) {
 module.exports = {
 	memes
 };
+
+//this is resolving any nested query between users and memes.
+//This is the solution to the null null null error. I think...
+//because it connects all the pieces in a many to one or many
+//to many relationship... which, maintains database truthfulness
+//Without this piece GRAPH ql is hardly a Graph.
